742007899fab97c60ae444990df55fc2969353ec
Sreejib-Nandy <sreejibnandy2518@gmail.com>
2026-05-13 23:49:46 +0530

    fix

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -4,7 +4,7 @@ export const sendEmail = async ({ to, subject, html }) => {
   try {
     const transporter = nodemailer.createTransport({
       host: "smtp-relay.brevo.com",
-      port: 587,
+      port: 2525,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
